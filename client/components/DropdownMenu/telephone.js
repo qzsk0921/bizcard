28c641041dc4ec4dbbc55479f92f69e93a9c3ad3
sunkang <153252148@qq.com>
2022-01-04 18:05:35 +0800

    202101041805

---
+++ client/components/DropdownMenu/telephone.js
@@ -0,0 +1,112 @@
+// components/DropdownMenu/telephone.js
+import store from '../../store/common'
+import create from '../../utils/create'
+
+// Component({
+create({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    opened: {
+      type: Number,
+      value: 0
+    },
+    telephoneObj: Object
+  },
+  observers: {
+    'opened': function (val) {
+      // console.log(val)
+      if (val === 1) {
+        const query = wx.createSelectorQuery().in(this)
+        query.select('.dropdown-item-down__content').boundingClientRect(rect => {
+          console.log(rect)
+
+          // 如果是switchTab页面，加tabbar高度
+          const tabbarRoutes = this.getTabBar().data.list
+          const currentRoute = getCurrentPages()[getCurrentPages().length - 1].route
+          const res = tabbarRoutes.some(item =>
+            item.pagePath === currentRoute
+          )
+
+          let height = rect.height
+          if (res) {
+            height += store.data.compatibleInfo.tabbarH
+          }
+
+          this.setData({
+            height
+          })
+        }).exec()
+      }
+    }
+  },
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    height: 0,
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    dropdownItemTapHandle(e) {
+      const type = e.target.dataset.type
+      if (type === 'copy') {
+        // 复制
+        this.copyToClipboard(this.data.telephoneObj.content, '已为您存至粘贴板')
+      } else if (type === 'call') {
+        // 拨打
+        wx.makePhoneCall({
+          phoneNumber: this.data.telephoneObj.content,
+        })
+      } else if (type === 'create') {
+        // 创建通讯录新联系人
+
+        // 用户可以选择将该表单以「新增联系人」或「添加到已有联系人」的方式，写入手机系统通讯录。
+        // 访问蓝牙、 添加通讯录联系人、 添加日历事件需要用户授权
+        // 为优化用户体验， 平台将于2022年2月21日24时之后对访问蓝牙、 添加通讯录联系人、 添加日历事件增加用户授权。 如有使用该技术服务， 请开发者及时对小程序进行调整， 避免影响服务流程。 查看详情: https: //developers.weixin.qq.com/community/develop/doc/000e881c7046a8fa1f4d464105b001
+          wx.addPhoneContact({
+            firstName: '孙'
+          })
+      } else if (type === 'add') {
+        // 添加到通讯录现有联系人
+        wx.addPhoneContact({
+          firstName: '孙'
+        })
+      } else if (type === 'cancel') {
+        // 取消
+        this.setData({
+          opened: 0
+        })
+      }
+    },
+    // 复制到剪贴板
+    copyToClipboard(data, title) {
+      title ? title : '复制到剪贴板'
+      wx.setClipboardData({
+        data,
+        success: (res) => {
+          wx.showToast({
+            title,
+            icon: 'none'
+          })
+        },
+      })
+    },
+  },
+  lifetimes: {
+    ready() {
+      // 在组件在视图层布局完成后执行
+    },
+    attached: function () {
+
+      // 在组件实例进入页面节点树时执行
+    },
+    detached: function () {
+      // 在组件实例被从页面节点树移除时执行
+    },
+  },
+})
--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -13,6 +13,7 @@ create(store, {
    * 页面的初始数据
    */
   data: {
+    is_select: 0, //协议
     isOverShare: true,
 
     userInfo: null,
@@ -20,9 +21,9 @@ create(store, {
     compatibleInfo: null, //navHeight menuButtonObject systemInfo isIphoneX isIphone
     tabbarH: null,
 
-    navStatus: 'isEmpty',
+    navStatus: 'isEntryWithShare', //isEmpty
 
-    isCardEmpty: 1,
+    isCardEmpty: 0,
     // swiper
     background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
     indicatorDots: true,
@@ -30,9 +31,111 @@ create(store, {
     interval: 2000,
     autoplay: true,
     duration: 500,
-    indicatorActiveColor: '#4980F9'
+    indicatorActiveColor: '#4980F9',
+    section4: [{
+      id: 1,
+      img: '/assets/images/card_phone.png',
+      title: '电话',
+      content: '136-0603-2423',
+      toast: '',
+    }, {
+      id: 2,
+      img: '/assets/images/card_email.png',
+      title: '邮箱',
+      content: '13606032423@qq.com',
+      toast: '已为您复制Ta的邮箱至您的粘贴板',
+    }, {
+      id: 3,
+      img: '/assets/images/card_location.png',
+      title: '地址',
+      content: '深圳市福田区新一代产业园1栋30楼',
+      toast: '已将地址存至手机粘贴板' //然后，跳转至地图导航
+    }, {
+      id: 4,
+      img: '/assets/images/card_call.png',
+      title: '座机',
+      content: '0592-5923912'
+    }],
+    dialog: {
+      opened: 0,
+      telephoneObj: ''
+    }
   },
+  // 选择阅读协议
+  agreementHandle() {
+    this.setData({
+      is_select: !this.data.is_select
+    })
+  },
+  // 关闭购物车弹窗
+  dropdownTelephoneMaskTap() {
+    this.setData({
+      'dialog.opened': 0
+    })
+  },
+  // 电话 邮箱 地址 座机
+  itemHandle(e) {
+    const item = e.currentTarget.dataset.item
 
+    if (item.toast) {
+      // 邮箱 地址
+      this.copyHandle(item.content, item.toast)
+
+      if (item.id === 3) {
+        // 地址
+        setTimeout(() => {
+          // wx.chooseLocation({
+          //   success: res => {
+          //     that.getValueMap(res)
+          //   },
+          //   fail: res => {
+          //     console.log('打开地图选择位置取消', res)
+          //   },
+          //   complete: res => {
+          //     // 接口调用结束的回调函数（调用成功、失败都会执行）
+          //     console.log('chooseLocation complete')
+          //     console.log(res)
+          //   }
+          // })
+          wx.openLocation({
+            latitude: 24.44579,
+            longitude: 118.08243
+          })
+        }, 100)
+      }
+    } else {
+      // 电话 座机
+
+      // if (item.id === 1) {
+      //   // 电话
+
+      // } else if (item.id === 4) {
+      //   // 座机
+      // }
+
+      this.setData({
+        'dialog.telephoneObj': item,
+        'dialog.opened': 1
+      })
+    }
+  },
+  // 复制订单编号
+  copyHandle(data, title) {
+    this.copyToClipboard(data, title)
+  },
+  // 复制到剪贴板
+  copyToClipboard(data, title) {
+    title ? title : '复制到剪贴板'
+    wx.setClipboardData({
+      data,
+      success: (res) => {
+        wx.showToast({
+          title,
+          icon: 'none'
+        })
+      },
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
