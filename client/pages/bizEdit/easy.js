3f3dd18991afba50a11ecaa363b440f5412886eb
sunkang <153252148@qq.com>
2022-01-07 18:03:46 +0800

    202101071803

--- client/pages/bizEdit/easy.js
+++ client/pages/bizEdit/easy.js
@@ -1,5 +1,12 @@
 // pages/bizEdit/easy.js
-Page({
+import store from '../../store/common'
+import create from '../../utils/create'
+import {
+  checkMobile
+} from '../../utils/util'
+
+// Page({
+create(store, {
 
   /**
    * 页面的初始数据
@@ -10,7 +17,116 @@ Page({
     systemInfo: null,
     compatibleInfo: null, //navHeight menuButtonObject systemInfo isIphoneX isIphone
   },
+  getPhoneNumber(e) {
+    console.log(e)
+    const _this = this
+    if (e.detail.encryptedData) {
+      this.updatePhone({
+        encryptedData: e.detail.encryptedData,
+        iv: e.detail.iv,
+      }).then(res => {
+        const data = res.data.phone
+        console.log(data)
+        _this.setData({
+          'userInfo.phone': data,
+          'phone': data,
+        })
+        app.globalData.userInfo['phone'] = data //服务器解密后反回
+        wx.showToast({
+          title: '绑定成功，请重新点击立即购买',
+          icon: 'none'
+        })
+      }).catch(res => {
+        console.log(res)
+      })
+    } else {
+      wx.showModal({
+        content: '为便于商家服务需要您进行手机号授权',
+        // confirmText: '同意',
+        confirmText: '确定',
+        confirmColor: '#4283FB',
+        showCancel: false,
+        // cancelText: '拒绝',
+        // cancelColor: '#999999',
+        success(res) {
+          if (res.confirm) {
+            console.log('确定')
+          } else if (res.cancel) {
+            console.log('取消')
+          }
+        }
+      })
+    }
+  },
+  // 自动填入手机号
+  autoPhoneNumber() {
+    this.setData({
+      phone: this.data.userInfo.phone
+    })
+  },
+  // 保存名片信息
+  formSubmit(e) {
+    console.log(e)
+    const formdata = e.detail.value
+    const res = Object.keys(formdata).some(key => !formdata[key])
 
+    if (res) {
+      wx.showToast({
+        icon: 'none',
+        title: '请填写完整信息',
+      })
+    } else {
+      // 校验手机号
+      if (!checkMobile(formdata.consignee_phone)) {
+        wx.showToast({
+          title: '请输入正确手机号',
+          icon: 'none'
+        })
+        return false
+      }
+
+      const mydata = {
+        ...this.data.deliveryAddress,
+        ...formdata
+      }
+
+      if (this.data.type === 'edit') {
+        // 编辑
+        mydata.id = this.data.id
+        this.editAddress(mydata).then(res => {
+          wx.showToast({
+            icon: 'none',
+            title: res.msg,
+            duration
+          })
+
+          setTimeout(() => {
+            wx.navigateBack({
+              delta: 0,
+            })
+          }, duration)
+        })
+      } else {
+        // 新增
+        this.addAddress(mydata).then(res => {
+          wx.showToast({
+            icon: 'none',
+            title: res.msg,
+            duration
+          })
+
+          setTimeout(() => {
+            // wx.navigateTo({
+            //   url: '/pages/location/index/index',
+            // })
+            wx.navigateBack({
+              delta: 0,
+            })
+          }, duration)
+        })
+      }
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
    */
--- client/pages/bizEdit/edit.js
+++ client/pages/bizEdit/edit.js
@@ -1,13 +1,86 @@
 // pages/bizEdit/edit.js
-Page({
+import store from '../../store/common'
+import create from '../../utils/create'
+
+// Page({
+create(store, {
 
   /**
    * 页面的初始数据
    */
   data: {
+    cardData: {
+      avatar: '', //头像
+      video: '', //视频
+      logo: ''
+    },
+    currentCountProfile: 0,
+    currentCountTag: 0,
+    currentCountIntroduction: 0, //公司简介
 
+    userInfo: null,
+    navigationBarTitleText: '编辑信息',
+    systemInfo: null,
+    compatibleInfo: null, //navHeight menuButtonObject systemInfo isIphoneX isIphone
   },
 
+  chooseImage(field) {
+    const that = this
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success(res) {
+        // tempFilePath可以作为img标签的src属性显示图片
+        const tempFilePaths = res.tempFilePaths;
+        console.log(tempFilePaths)
+        that.setData({
+          [`cardData.${field}`]: tempFilePaths[0]
+        })
+      }
+    })
+  },
+  // 选择头像
+  inputAvatarHandle() {
+    this.chooseImage('avatar')
+  },
+  // 选择视频
+  inputVideoHandle() {
+    const that = this
+    wx.chooseVideo({
+      sourceType: ['album', 'camera'],
+      maxDuration: 60,
+      camera: 'back',
+      success(res) {
+        // console.log(res.tempFilePath)
+        that.setData({
+          'cardData.video': res.tempFilePath
+        })
+      }
+    })
+  },
+  // 选择logo图
+  inputLogoHandle() {
+    this.chooseImage('logo')
+  },
+
+  textareaInputHandle(num, type) {
+    let len = e.detail.value.length
+
+    if (len > num) {
+      len = num
+    }
+    // const data = {
+    //   type: len
+    // }
+    // this.setData(data)
+  },
+  textareaInputProfileHandle() {
+    this.textareaInputHandle(100, 'currentCountProfile')
+  },
+  textareaInputIntroductionHandle() {
+    this.textareaInputHandle(300, 'currentCountIntroduction')
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -26,7 +99,17 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    if (!this.data.compatibleInfo.navHeight) {
+      this.setData({
+        compatibleInfo: this.store.data.compatibleInfo
+      })
+    }
 
+    if (!this.data.userInfo) {
+      this.setData({
+        userInfo: this.store.data.userInfo
+      })
+    }
   },
 
   /**
